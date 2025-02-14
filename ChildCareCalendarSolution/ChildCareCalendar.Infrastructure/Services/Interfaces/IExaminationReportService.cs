2e5b5154e1bc6b92f2804c74a0877a8c0c430b01
khaiqui <nguyenkhaiqui2003@gmail.com>
2025-02-14 13:40:37 +0700

    Update medicine function

---
+++ ChildCareCalendarSolution/ChildCareCalendar.Infrastructure/Services/Interfaces/IExaminationReportService.cs
@@ -0,0 +1,16 @@
+﻿using ChildCareCalendar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildCareCalendar.Infrastructure.Services.Interfaces
+{
+    public interface IExaminationReportService
+    {
+        Task<IEnumerable<ExaminationReport>> GetAllExaminationReportsAsync();
+        Task<ExaminationReport> GetExaminationReportByIdAsync(int id);
+        Task CreateExaminationReportAsync(ExaminationReport examinationReport);
+    }
+}
---
+++ ChildCareCalendarSolution/ChildCareCalendar.Infrastructure/Services/Interfaces/IPrescriptionDetailService.cs
@@ -0,0 +1,17 @@
+﻿using ChildCareCalendar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildCareCalendar.Infrastructure.Services.Interfaces
+{
+    public interface IPrescriptionDetailService
+    {
+        Task<IEnumerable<PrescriptionDetail>> GetAllPrescriptionDetailAsync();
+        Task<PrescriptionDetail> GetPrescriptionDetailByIdAsync(int id);
+        Task CreatePrescriptionDetailAsync(PrescriptionDetail prescriptionDetail);
+      
+    }
+}
