1b2b8205d1c28be32eba34e3cce9c3b914297b2c
quannmin <laogiado2003@gmail.com>
2025-02-26 20:44:52 +0700

    update service findAsync

---
+++ ChildCareCalendarSolution/ChildCareCalendar.Infrastructure/Services/ChildrenRecordService.cs
@@ -0,0 +1,43 @@
+﻿using ChildCareCalendar.Domain.Entities;
+using ChildCareCalendar.Infrastructure.Repository;
+using System.Linq.Expressions;
+
+namespace ChildCareCalendar.Infrastructure.Services
+{
+    public class ChildrenRecordService
+    {
+        private readonly IRepository<ChildrenRecord> _childrenRecordRepository;
+
+        public ChildrenRecordService(IRepository<ChildrenRecord> childrenRecordRepository)
+        {
+            _childrenRecordRepository = childrenRecordRepository;
+        }
+
+        public async Task AddChildrenRecordAsync(ChildrenRecord childrenRecord)
+        {
+            await _childrenRecordRepository.AddAsync(childrenRecord);
+        }
+
+        public async Task<int> DeleteChildrenRecordAsync(int id)
+        {
+            var childrenRecord = await _childrenRecordRepository.GetByIdAsync(id);
+            if (childrenRecord != null)
+            {
+                childrenRecord.IsDelete = true;
+                await _childrenRecordRepository.UpdateAsync(childrenRecord, childrenRecord.Id);
+                id = childrenRecord.Id;
+            }
+            return id > 0 ? id : 0;
+        }
+        public async Task<IEnumerable<ChildrenRecord>> FindUsersAsync(Expression<Func<ChildrenRecord, bool>> predicate,
+                                                                      params Expression<Func<ChildrenRecord, object>>[] includes)
+        {
+            return await _childrenRecordRepository.FindAsync(predicate, includes);
+        }
+
+        public async Task UpdateChildrenRecordAsync(ChildrenRecord childrenRecord)
+        {
+            await _childrenRecordRepository.UpdateAsync(childrenRecord, childrenRecord.Id);
+        }
+    }
+}
