4a04b849aba333104f94df61e7a8f19d9a93a3d4
khaiqui <nguyenkhaiqui2003@gmail.com>
2025-03-01 19:21:42 +0700

    Save code before pull

---
+++ ChildCareCalendarSolution/ChildCareCalendar.Domain/ViewModels/ExaminationReport/ExaminationReportViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildCareCalendar.Domain.ViewModels.ExaminationReport
+{
+    public class ExaminationReportViewModel
+    {
+        public int Id { get; set; }
+        public int ChildrenRecordId { get; set; }
+        public string? ChildrenName { get; set; }
+        public int AppointmentId { get; set; }
+        public DateTime ExamDate { get; set; }
+        public string? Diagnosis { get; set; }
+        public bool IsDeleted { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
