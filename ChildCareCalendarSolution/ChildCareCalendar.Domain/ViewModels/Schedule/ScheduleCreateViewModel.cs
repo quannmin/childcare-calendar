28c4f6dc43bb5168bbaff5519b885f7b035261f2
N-I-V-Z <vothanhnhan106@gmail.com>
2025-03-28 13:36:36 +0700

    fix table header speciality workhour and validation schedule admin page

--- ChildCareCalendarSolution/ChildCareCalendar.Domain/ViewModels/Schedule/ScheduleCreateViewModel.cs
+++ ChildCareCalendarSolution/ChildCareCalendar.Domain/ViewModels/Schedule/ScheduleCreateViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace ChildCareCalendar.Domain.ViewModels.Schedule
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChildCareCalendar.Domain.ViewModels.Schedule
 {
 	public class ScheduleCreateViewModel
     {
         public DateTime? WorkDay { get; set; }
         public string? day { get; set; }
-        public int WorkHourId { get; set; }
-        public int UserId { get; set; }
+		[Required(ErrorMessage = "Hãy chọn Giờ làm việc")]
+		public int? WorkHourId { get; set; }
+		[Required(ErrorMessage = "Hãy chọn Bác sĩ")]
+		public int? UserId { get; set; }
     }
 }
